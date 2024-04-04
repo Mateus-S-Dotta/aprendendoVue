cbdeec50aac8042a70da171b27762e3f5c398573
Davi Dotta <mateusalaibbdotta@gmail.com>
2024-04-04 01:13:15 -0300

    pokedex screen

--- src/components/PokemonVue.vue
+++ src/components/PokemonVue.vue
@@ -8,57 +8,60 @@
         <h3>Clique em qualquer lugar na pokedex para interagir</h3>
         <div class="center">
             <img ref="image" @click="ativarVisualicao" @load="getImageSize" :src="imgPokedex" />
-            <div class="tela" :style="boxStyle">
+            <div :style="boxStyle">
+                <div v-if="pokemons[selected - 1]" class="screenPart texts">
+                    <h1 :style="{fontSize: imageSize.height / 15.5 + 'px'}">{{ pokemons[selected - 1].name}}</h1>
+                </div>
+                <div class="screenPart mainText texts">
+                    <h1 :style="{fontSize: imageSize.height / 15.5 + 'px'}">{{ pokemons[selected].name ? pokemons[selected].name : '' }}</h1>
+                </div>
+                <div v-if="pokemons[selected + 1]" class="screenPart lastText texts">
+                    <h1 :style="{fontSize: imageSize.height / 15.5 + 'px'}">{{ pokemons[selected + 1].name }}</h1>
+                </div>
             </div>
             <div :style="{
-                    ...pokedexBotao,
-                    marginRight: (imageSize.width / 1.75) + 'px',
-                    backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
-                }"
-            >
-                <h1 :style="{opacity: visualizador}" class="text">1</h1>
+                ...pokedexBotao,
+                marginRight: (imageSize.width / 1.75) + 'px',
+                backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
+            }">
+                <h1 :style="{ opacity: visualizador }" class="text">1</h1>
             </div>
             <div :style="{
-                    ...pokedexBotao, 
-                    marginRight: (imageSize.width / 28) + 'px', 
-                    backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
-                }"
-            >
-                <h1 :style="{opacity: visualizador}" class="text">1</h1>
+                ...pokedexBotao,
+                marginRight: (imageSize.width / 28) + 'px',
+                backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
+            }">
+                <h1 :style="{ opacity: visualizador }" class="text">1</h1>
             </div>
             <div :style="{
-                    ...setasH,
-                    marginLeft: (this.imageSize.height / 2.05) + 'px',
-                    backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
-                }"
-            >
-                <h2 :style="{opacity: visualizador}" class="text">3</h2>
+                ...setasV,
+                bottom: (this.imageSize.height / 5) + 'px',
+                backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
+            }" @click="handlePokemon(false)">
+                <h2 :style="{ opacity: visualizador }" class="text">2</h2>
             </div>
             <div :style="{
-                    ...setasH,
-                    marginLeft: (this.imageSize.height / 3.55) + 'px',
-                    backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
-                }"
-            >
-                <h2 :style="{opacity: visualizador}" class="text">2</h2>
+                ...setasH,
+                marginLeft: (this.imageSize.height / 3.55) + 'px',
+                backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
+            }" @click="handlePokemon(false)">
+                <h2 :style="{ opacity: visualizador }" class="text">2</h2>
             </div>
             <div :style="{
-                    ...setasV,
-                    bottom: (this.imageSize.height / 9.8) + 'px',
-                    backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
-                }"
-            >
-                <h2 :style="{opacity: visualizador}" class="text">3</h2>
+                ...setasH,
+                marginLeft: (this.imageSize.height / 2.05) + 'px',
+                backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
+            }" @click="handlePokemon(true)">
+                <h2 :style="{ opacity: visualizador }" class="text">3</h2>
             </div>
             <div :style="{
-                    ...setasV,
-                    bottom: (this.imageSize.height / 5) + 'px',
-                    backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
-                }"
-            >
-                <h2 :style="{opacity: visualizador}" class="text">2</h2>
+                ...setasV,
+                bottom: (this.imageSize.height / 9.8) + 'px',
+                backgroundColor: `rgb(100, 100, 255, ${visualizador})`,
+            }" @click="handlePokemon(true)">
+                <h2 :style="{ opacity: visualizador }" class="text">3</h2>
             </div>
-            <div class="instruções" :style="{opacity: visualizador * 1.5}">
+            <div class="instruções" :style="{ opacity: visualizador * 1.5 }">
                 <div class="line">
                     <h3>1:</h3>
                     <p>Alterar Modo</p>
@@ -77,17 +80,17 @@
 </template>
 
 <script>
-    import pokedex from '../assets/pokedex.png'
-    // import axios from 'axios';
+    import pokedex from '../assets/pokedex.png';
+    import axios from 'axios';
 
     export default {
         name: "PokemonVue",
         data() {
             return {
                 visualizador: 0,
-                limit: 10,
+                selected: 0,
                 offset: 0,
-                pokemon: '',
+                pokemons: [{}],
                 imgPokedex: pokedex,
                 imageSize: { width: 0, height: 0 },
                 boxStyle: {},
@@ -97,15 +100,9 @@
                 setasV: {},
             }
         },
-        // created: async function () {
-        //     try {
-        //         const dados = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${this.offset}`);
-        //         const pokemon1 = await axios.get(dados.data.results[0].url);
-        //         this.pokemon = pokemon1.data.sprites.front_default
-        //     } catch (error) {
-        //         console.log(error)
-        //     }
-        // },
+        created() {
+            this.callPokemons(true);
+        },
         methods: {
             getImageSize() {
                 const img = this.$refs.image;
@@ -118,15 +115,51 @@
             ativarVisualicao() {
                 clearInterval(this.intervalo);
                 this.visualizador = 0.7;
-                setTimeout(() => {
-                    this.intervalo = setInterval(() => {
-                        this.visualizador = this.visualizador - 0.05
-                        if (this.visualizador <= 0) {
-                            this.visualizador = 0;
-                            clearInterval(this.intervalo);
+                this.intervalo = setInterval(() => {
+                    this.visualizador = this.visualizador - 0.05
+                    if (this.visualizador <= 0) {
+                        this.visualizador = 0;
+                        clearInterval(this.intervalo);
+                    }
+                }, 200);
+            },
+            handlePokemon(more) {
+                if (more) {
+                    if (!this.pokemons[this.selected + 1]) {
+                        return
+                    }
+                    this.selected = this.selected + 1;
+                    if ((this.selected === 7 && this.pokemons.length === 10) || this.selected === 17) {
+                        this.offset = this.offset + 10;
+                        this.callPokemons(this.selected);
+                    }
+                } else if (this.selected !== 0) {
+                    this.selected = this.selected - 1;
+                    if(this.selected === 2 && this.pokemons[this.selected].name !== 'venusaur' ) {
+                        this.offset = this.offset - 10;
+                        this.callPokemons(this.selected);
+                    }
+                }
+            },
+            callPokemons: async function (initial) {
+                    try {
+                        const dados = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${this.offset}`);
+                        if(initial === true) {
+                            this.pokemons = [...dados.data.results];
+                        } else if (initial === 17) {
+                            this.pokemons.splice(0, 10);
+                            this.pokemons = [...this.pokemons, ...dados.data.results];
+                            this.selected = 7;
+                        } else if (initial === 7) {
+                            this.pokemons = [...this.pokemons, ...dados.data.results];
+                        } else if (initial === 2) {
+                            this.pokemons.splice(-10, 10);
+                            this.pokemons = [ ...dados.data.results, ...this.pokemons];
+                            this.selected = 12;
                         }
-                    }, 150);
-                }, 1000)
+                    } catch (error) {
+                        console.log(error)
+                    }
             }
         },
         mounted() {
@@ -143,13 +176,14 @@
                         height: (this.imageSize.height / 2.35) + 'px',
                         width: (this.imageSize.height / 2.35) + 'px',
                         position: 'absolute',
-                        backgroundColor: 'rgba(0, 0, 0, 0.9)',
+                        backgroundColor: '#fff',
+                        boxShadow: 'inset 1px 1px 10px rgba(0, 0, 0, 0.5)',
                         top: '24.5%',
                         marginLeft: this.imageSize.width / 10.5 + 'px',
                     };
                     this.pokedexBotao = {
                         display: 'flex',
-                        justifyContent : 'center',
+                        justifyContent: 'center',
                         alignItems: 'center',
                         height: (this.imageSize.height / 11) + 'px',
                         width: (this.imageSize.height / 11) + 'px',
@@ -160,7 +194,7 @@
                     };
                     this.setasH = {
                         display: 'flex',
-                        justifyContent : 'center',
+                        justifyContent: 'center',
                         alignItems: 'center',
                         position: 'absolute',
                         height: (this.imageSize.height / 18) + 'px',
@@ -170,7 +204,7 @@
                     };
                     this.setasV = {
                         display: 'flex',
-                        justifyContent : 'center',
+                        justifyContent: 'center',
                         alignItems: 'center',
                         position: 'absolute',
                         height: (this.imageSize.width / 16) + 'px',
@@ -217,5 +251,31 @@
         gap: 16px;
         position: absolute;
         top: 105%
+    }
+
+    .screenPart {
+        height: 33.33%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-transform: capitalize;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+
+    .texts {
+        position: absolute;
+        left: 50%;
+        right: 50%;
+    }
+
+    .mainText {
+        color: red;
+        top: 33.333%;
+        text-decoration: underline;
+    }
+
+    .lastText {
+        top: 66.666%
     }
 </style>
